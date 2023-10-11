e45284e94b94f703f634e1ab270de06eb46601b6
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-10-10 20:30:59 -0400

    Changed metadata title and description + added alt tags to images in projects section

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -5,21 +5,45 @@ import "slick-carousel/slick/slick-theme.css";
 
 function Projects() {
   const carouselArray1 = [
-    { src: "/topview.jpg", alt: "Image 1" },
-    { src: "/blackbear1.jpeg", alt: "Image 2" },
-    { src: "/blackbear2.jpeg", alt: "Image 3" },
+    {
+      src: "/topview.jpg",
+      alt: "Whole view of a cafe and restaurants collection in Cairo, Egypt",
+    },
+    {
+      src: "/blackbear1.jpeg",
+      alt: "Blackbear American Burger entrance design in Cairo, Egypt",
+    },
+    {
+      src: "/blackbear2.jpeg",
+      alt: "Blackbear American Burger entrance design in Cairo, Egypt",
+    },
   ];
 
   const carouselArray2 = [
-    { src: "/tabak.jpg", alt: "Image 1" },
-    { src: "/cafemilano.jpg", alt: "Image 2" },
-    { src: "/cafemilano2.jpg", alt: "Image 3" },
+    { src: "/tabak.jpg", alt: "Interior design of Tabak cafe in Cairo, Egypt" },
+    {
+      src: "/cafemilano.jpg",
+      alt: "Interior design of Cafe Milano in Cairo, Egypt",
+    },
+    {
+      src: "/cafemilano2.jpg",
+      alt: "Interior design of Cafe Milano in Cairo, Egypt",
+    },
   ];
 
   const carouselArray3 = [
-    { src: "/shawermer1.jpeg", alt: "Image 1" },
-    { src: "/nilegate1.jpeg", alt: "Image 2" },
-    { src: "/nilegate3.jpeg", alt: "Image 3" },
+    {
+      src: "/shawermer1.jpeg",
+      alt: "Shawermer entrance design in Cairo, Egypt",
+    },
+    {
+      src: "/nilegate1.jpeg",
+      alt: "Nile Gate entrance design in Cairo, Egypt",
+    },
+    {
+      src: "/nilegate3.jpeg",
+      alt: "Nile Gate entrance design in Cairo, Egypt",
+    },
   ];
   return (
     <section id="projects" className="text-gray-600 body-font">
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Three Brothers",
-  description: "Three Brothers Website",
+  title: "Three Brothers - Cafe Design & Carpentry Experts in Egypt",
+  description:
+    "Three Brothers: Your Trusted Cafe Design and Carpentry Experts in Egypt. Transforming your coffee shop dreams into reality with innovative cafe designs and expert carpentry services. Explore our website.",
 };
 
 export default function RootLayout({ children }) {
